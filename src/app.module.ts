84889729fea10ed915af023222b2f57c012a441c
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-03-04 21:36:10 +0900

    [CHORE] 정리 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MessageModule } from './rabbitMQ-server/message.module';
 
 @Module({
-  imports: [
-    MessageModule,
-    ClientsModule.register([
-      {
-        name: 'MESSAGE_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672'], // RabbitMQ 서버 URL
-          queue: 'messages_queue', // 큐 이름
-          queueOptions: {
-            durable: false, // 메시지 내구성 여부
-          },
-        },
-      },
-    ]),
-  ],
-
-  providers: [],
-  controllers: [],
+  imports: [MessageModule],
 })
 export class AppModule {}
